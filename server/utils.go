79343f835bb11c53a62fda424496e854c75d6fa6
marius <marius@localhost.localdomain>
2017-05-02 17:51:18 +1200

    Add list contains util func

--- server/utils.go
+++ server/utils.go
@@ -6,3 +6,13 @@ import "gopkg.in/mgo.v2/bson"
 func NewId() string {
 	return bson.NewObjectId().Hex()
 }
+
+// Return if the given key is in the given list
+func ListContains(key string, values []string) bool {
+	for _, v := range values {
+		if v == key {
+			return true
+		}
+	}
+	return false
+}

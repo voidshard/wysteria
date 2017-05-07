34819c2678c32e9f8f533165c7c97a2c96901c8a
marius <marius@localhost.localdomain>
2017-05-07 16:43:58 +1200

    Switch examples 9 and 10

--- client/examples/10/main.go
+++ client/examples/10/main.go
@@ -6,24 +6,30 @@ import (
 )
 
 func main() {
-	// Example 10: Limit & offset
-	// Limiting query sizes & pagination are also possible and are achieved in the same manner you might expect.
-	// By default a new Search has a limit set to a default value (500 at the time of writing) with an offset
-	// of 0.
+	// Example 10: Deleting things
+	//  Deleting something will daisy-chain delete everything downstream too - all the way down to links.
+	//  But the things you link to wont be touched.
+	//
+	//  Deleting is slow & isn't to be considered atomic - it isn't intended that this operation be used
+	//  frequently, nor on realms that other people are using.
+	//
+	//  Whether something is hard or soft deleted is left to the implementations & configuration of the
+	//  data and searchbase layers.
 
 	client, err := wysteria.New()
 	if err != nil {
 		panic(err)
 	}
 
-	for i := 0; i < 3; i++ {
-		resources, err := client.Search().Name("default").Limit(1).Offset(i).FindResources()
-		if err != nil {
-			panic(err)
-		}
-		log.Println("Found", len(resources), "=>", resources[0].Name(), resources[0].Location())
+	collection, err := client.GetCollection("maps")
+	if err != nil {
+		panic(err)
 	}
-	// Found 1 => default url://images/oak01.png
-	// Found 1 => default /path/to/elm01.png
-	// Found 1 => default /path/to/pine01.png
+	log.Println("Delete maps:", collection.Delete())
+
+	collection, err = client.GetCollection("tiles")
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Delete tiles:", collection.Delete())
 }
